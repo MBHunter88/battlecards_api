3f7f4d48765d54382051bb22d03c96507ffd0a19
MBHunter88 <chellehunt88@gmail.com>
2024-08-07 12:06:59 -0700

    add time interval for stats to decrease

--- techtonica-projects/js-game/script.js
+++ techtonica-projects/js-game/script.js
@@ -5,28 +5,28 @@ let petHappiness = 70;
 
 //display initial stats
 function updateDisplay() {
-const healthStat = document.getElementById("healthText")
-const happinessStat = document.getElementById("happinessText"); 
+    const healthStat = document.getElementById("healthText")
+    const happinessStat = document.getElementById("happinessText");
 
-healthStat.innerHTML = petHealth;
-happinessStat.innerText = petHappiness; 
+    healthStat.innerHTML = petHealth;
+    happinessStat.innerText = petHappiness;
 }
 
 //implement actions to increase stats
 
 function increaseHealth() {
-if(petHealth < 100 && petHealth !=0) {
-    petHealth += 3;
-    //update display
-    updateDisplay()
-}
+    if (petHealth < 100 && petHealth != 0) {
+        petHealth += 3;
+        //update display
+        updateDisplay()
+    }
 }
 
 function increaseHappiness() {
-if(petHappiness < 100 && petHappiness != 0) {
-    petHappiness += 3;
-    updateDisplay();
-}
+    if (petHappiness < 100 && petHappiness != 0) {
+        petHappiness += 3;
+        updateDisplay();
+    }
 }
 
 
@@ -36,3 +36,18 @@ document.getElementById("button2").onclick = increaseHappiness;
 document.getElementById("button3").onclick = increaseHealth
 
 updateDisplay();
+
+//set time intervals to decrease stats
+//first create function to detemine decrease
+function decreaseStats() {
+    if(petHappiness > 0) {
+        petHappiness -= 1
+    }
+    if(petHealth > 0) {
+        petHealth -= 2
+    }
+
+    updateDisplay()
+}
+//set the time interval of decrease
+setInterval(decreaseStats, 3000); //adjust as needed 
