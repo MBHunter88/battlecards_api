ee57141d97f2a741b0610c3f81530bd5c7bd01f1
MBHunter88 <chellehunt88@gmail.com>
2024-08-07 13:44:35 -0700

    add logic for increasing both stats by clicking clean button

--- techtonica-projects/js-game/script.js
+++ techtonica-projects/js-game/script.js
@@ -2,7 +2,7 @@
 const petName = "";
 let petHealth = 70;
 let petHappiness = 70;
-let alertText = ""; 
+let alertText = "";
 let gameOverText = "";
 //display initial stats
 function updateDisplay() {
@@ -11,21 +11,21 @@ function updateDisplay() {
     const text = document.getElementById("text");
     healthStat.innerHTML = petHealth;
     happinessStat.innerText = petHappiness;
-     //update to include alerts for decreased stats
+    //update to include alerts for decreased stats
     lowStatAlert();
-  
-    //update to display text when game is over
-    gameOver(); 
-     // Combine alertText and gameOverText
-     let combinedText = alertText;
-     if (gameOverText !== "") {
-         combinedText += "<br>" + gameOverText; // Add a line break between messages
-     }
- 
-     // Update the text element with the combined message
-     text.innerHTML = combinedText;
 
-     clearStatDecreaseInterval();
+    //update to display text when game is over
+    gameOver();
+    // Combine alertText and gameOverText
+    let combinedText = alertText;
+    if (gameOverText !== "") {
+        combinedText += "<br>" + gameOverText; // Add a line break between messages
+    }
+
+    // Update the text element with the combined message
+    text.innerHTML = combinedText;
+
+    clearStatDecreaseInterval();
 }
 
 //implement actions to increase stats
@@ -45,21 +45,28 @@ function increaseHappiness() {
     }
 }
 
+function increaseBothbyCleaning() {
+    if(petHappiness < 100 && petHappiness != 0 || petHealth < 100 && petHealth != 0) {
+        petHappiness += 1;
+        petHealth += 1;
+        updateDisplay();
+    }
+}
 
 //call action functions on buttons
 document.getElementById("button1").onclick = increaseHealth;
 document.getElementById("button2").onclick = increaseHappiness;
-document.getElementById("button3").onclick = increaseHealth
+document.getElementById("button3").onclick = increaseBothbyCleaning; 
 
 updateDisplay();
 
 //set time intervals to decrease stats
 //first create function to detemine decrease
 function decreaseStats() {
-    if(petHappiness > 0) {
+    if (petHappiness > 0) {
         petHappiness -= 1
     }
-    if(petHealth > 0) {
+    if (petHealth > 0) {
         petHealth -= 2
     }
 
@@ -71,21 +78,21 @@ let statDecreaseInterval = setInterval(decreaseStats, 500); //adjust as needed f
 //set conditions for alerting user about poor health 
 
 function lowStatAlert() {
-     alertText = "";
-     if(gameOverText === "") {
-    if(petHappiness < 20 && petHappiness > 0) {
-       alertText = "Your pet is lonely! Click play to make them happy."
-       
-    } else if(petHealth < 20 && petHealth > 0) {
-        alertText = "Your pet is not feeling well! Feed and clean up their space to make them feel better."
-     } 
+    alertText = "";
+    if (gameOverText === "") {
+        if (petHappiness < 20 && petHappiness > 0) {
+            alertText = "Your pet is lonely! Click play to make them happy."
+
+        } else if (petHealth < 20 && petHealth > 0) {
+            alertText = "Your pet is not feeling well! Feed and clean up their space to make them feel better."
+        }
     }
 }
 
 //clear the interval for stat decrease if one stat reaches 0
 
-function clearStatDecreaseInterval(){
-    if(petHealth == 0 || petHappiness == 0) {
+function clearStatDecreaseInterval() {
+    if (petHealth == 0 || petHappiness == 0) {
         clearInterval(statDecreaseInterval)
     }
 }
@@ -93,11 +100,11 @@ function clearStatDecreaseInterval(){
 // set conditions for losing
 
 function gameOver() {
-     gameOverText = "";
-    if(petHappiness == 0) {
+    gameOverText = "";
+    if (petHappiness == 0) {
         gameOverText = "Your pet has run away"
     } else if (petHealth == 0) {
         gameOverText = "Your pet has died of starvation "
     } else
-    gameOverText = "";
+        gameOverText = "";
 }
