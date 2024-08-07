c7e5a3f3c4cec6e5f17906d60a863546b5fbe70d
MBHunter88 <chellehunt88@gmail.com>
2024-08-06 22:19:33 -0700

    initial setup and logic for buttons

---
+++ techtonica-projects/js-game/script.js
@@ -0,0 +1,38 @@
+//initialize variables for pet name and  initial stats
+const petName = "";
+let petHealth = 70;
+let petHappiness = 70;
+
+//display initial stats
+function updateDisplay() {
+const healthStat = document.getElementById("healthText")
+const happinessStat = document.getElementById("happinessText"); 
+
+healthStat.innerHTML = petHealth;
+happinessStat.innerText = petHappiness; 
+}
+
+//implement actions to increase stats
+
+function increaseHealth() {
+if(petHealth < 100 && petHealth !=0) {
+    petHealth += 3;
+    //update display
+    updateDisplay()
+}
+}
+
+function increaseHappiness() {
+if(petHappiness < 100 && petHappiness != 0) {
+    petHappiness += 3;
+    updateDisplay();
+}
+}
+
+
+//call action functions on buttons
+document.getElementById("button1").onclick = increaseHealth;
+document.getElementById("button2").onclick = increaseHappiness;
+document.getElementById("button3").onclick = increaseHealth
+
+updateDisplay();
